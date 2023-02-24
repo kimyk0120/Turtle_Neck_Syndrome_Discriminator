c443badbeb275adb4a1762e2b0f0004b45ebda3d
kimyk0120 <kimyk0120@hotmail.com>
2023-02-24 15:03:38 +0900

    * init webcam

--- data_collector.py
+++ data_collector.py
@@ -1,6 +1,12 @@
 import dlib
 import mediapipe
 import cv2
+import numpy as np
+import time
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("./dat/shape_predictor_68_face_landmarks.dat")
+
 
 webcam = cv2.VideoCapture(0)
 
@@ -8,12 +14,21 @@ if not webcam.isOpened():
     print("Could not open webcam")
     exit()
 
-
+prevTime = 0
 while webcam.isOpened():
     status, frame = webcam.read()
 
+    curTime = time.time()
+    sec = curTime - prevTime
+    prevTime = curTime
+
+    fps = 1 / (sec)
+    print("FPS: %0.1f" % fps)
+
     if status:
+
         # cv2.imshow("test", frame)
+
         pass
 
 
